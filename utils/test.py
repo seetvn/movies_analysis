228dec32cc1dd385872ff8869b0f9d268eac5313
seetvn <waiyansteven2002@gmail.com>
2025-02-18 17:31:27 +0000

    tests stuff

--- utils/test.py
+++ utils/test.py
@@ -1,16 +1,5 @@
 import pandas as pd
 from dataframe_functions import generate_movies_xlsx
-# data = pd.read_excel("/home/seetvn/random_projects/ekimetrics/data/movies_str.xlsx")
-
-# data = format_df(data,verbose=True)
-# print('===== step1: done ====== \n')
-# data = drop_invalid_NaN_boxoffice_values(data,verbose=True)
-# print('==== step2: done ====== \n')
-# indices = data[data['BoxOffice'].isna()].index
-# data = refactor_valid_NaN_boxoffice_values(data,verbose=True)
-# print(data.loc[indices])
-# print('==== step3: done ====== \n')
-
-df = generate_movies_xlsx(verbose=True)
-
-print(df.head())
+df = pd.read_excel("/home/seetvn/random_projects/ekimetrics/data/movies_formatted.xlsx")
+print(df.dtypes)
+print(df['Genre'].unique)
