5c62ed3bd16e486d89b64d1d5e049cbaaef5a428
seetvn <waiyansteven2002@gmail.com>
2025-02-20 14:19:03 +0000

    test.py

--- utils/test.py
+++ utils/test.py
@@ -1,6 +1,11 @@
 import pandas as pd
-from dataframe_functions import generate_genre_one_hot_encodings_df
+from dataframe_functions import generate_one_hot_encodings_df
 df = pd.read_excel("/home/seetvn/random_projects/ekimetrics/data/formatted/movies_formatted.xlsx")
+print(df.columns)
 
-df = generate_genre_one_hot_encodings_df(df)
-print(df.loc[11])
+# print(df.head(12))
+# print(df.loc[((df['Search Trend'].isna() != True) & (df['Year']==2010)),['Title','imdbRating','BoxOffice','Search Trend']])
+
+df = generate_one_hot_encodings_df(df)
+print(df.head(9))
+
